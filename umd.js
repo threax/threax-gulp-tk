b250b547866c685101e8672c954947a3ec72fdc6
Andrew Piper <threax@users.noreply.github.com>
2016-10-01 16:49:11 -0400

    Added umd and bumped version.

---
+++ umd.js
@@ -0,0 +1,65 @@
+﻿var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    rename = require("gulp-rename"),
+    sourcemaps = require("gulp-sourcemaps");
+
+var wrapUmd = require('jsns/umd-wrapper.js');
+
+//Minification
+function compileJavascript(settings) {
+    if (settings.minify && settings.concat) {
+        minifyConcatJs(settings);
+        concatJs(settings);
+    }
+    else if (settings.minify) {
+        minifyEachJs(settings);
+    }
+    else if (settings.concat) {
+        concatJs(settings);
+    }
+    else {
+        copyEachJs(settings);
+    }
+}
+module.exports = compileJavascript;
+
+function minifyConcatJs(settings) {
+    return gulp.src(settings.libs, { base: settings.base })
+        .pipe(sourcemaps.init())
+        .pipe(wrapUmd(settings))
+        .pipe(concat(settings.output + '.js'))
+        .pipe(uglify())
+        .pipe(rename(settings.output + '.min.js'))
+        .pipe(sourcemaps.write(".", { includeContent: false, sourceRoot: settings.sourceRoot }))
+        .pipe(gulp.dest(settings.dest));
+};
+
+function minifyEachJs(settings) {
+    gulp.src(settings.libs, { base: settings.base })
+        .pipe(sourcemaps.init())
+        .pipe(wrapUmd(settings))
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.extname = ".min.js";
+        }))
+        .pipe(sourcemaps.write(".", { includeContent: false, sourceRoot: settings.sourceRoot }))
+        .pipe(gulp.dest(settings.dest));
+};
+
+function concatJs(settings) {
+    return gulp.src(settings.libs, { base: settings.base })
+        .pipe(sourcemaps.init())
+        .pipe(wrapUmd(settings))
+        .pipe(concat(settings.output + '.js'))
+        .pipe(sourcemaps.write(".", { includeContent: false, sourceRoot: settings.sourceRoot }))
+        .pipe(gulp.dest(settings.dest));
+};
+
+function copyEachJs(settings) {
+    gulp.src(settings.libs, { base: settings.base })
+        .pipe(sourcemaps.init())
+        .pipe(wrapUmd(settings))
+        .pipe(sourcemaps.write(".", { includeContent: false, sourceRoot: settings.sourceRoot }))
+        .pipe(gulp.dest(settings.dest));
+};
